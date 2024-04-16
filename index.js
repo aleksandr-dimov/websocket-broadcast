7d696339bb7045ea130d2fe9d14860eefc3a2761
Александр Димов <adimov@arboost.ru>
2024-04-16 16:29:02 +0300

    Websocket server

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs')
+var https = require('https')
+var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+//... bunch of other express stuff here ...
+
+//pass in your express app and credentials to create an https server
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({
+    server: httpsServer
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+wss.on('connection', function connection(ws) {
+    wss.clients.forEach((client) => {
+        client.send(client.clients)
+    });
+    ws.on('message', function incoming(data, isBinary) {
+        const message = isBinary ? data : data.toString();
+        wss.clients.forEach((client) => {
+            client.send(message)
+        });
+    });
+});
