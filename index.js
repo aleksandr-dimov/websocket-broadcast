7f71b87c3c1333301a5afda780b70c6f3bb33f47
aleksandr-dimov <167210238+aleksandr-dimov@users.noreply.github.com>
2024-04-16 19:12:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-var fs = require('fs')
-var https = require('https')
-var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
-var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+var http = require('http')
 
-var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 
 //... bunch of other express stuff here ...
 
-//pass in your express app and credentials to create an https server
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(10000);
+var httpServer = http.createServer(app);
+httpServer.listen(10000);
 
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({
-    server: httpsServer
+    server: httpServer
 });
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 wss.on('connection', function connection(ws) {
     wss.clients.forEach((client) => {
